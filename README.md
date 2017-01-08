51bed0f8a48768a39d776c59457e3469ccc00b45
Alexander Shepetko <a@shepetko.com>
2017-01-08 02:22:35 +0200

    Release 0.3

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-# PytSite LiveJournal Plugin
+# PytSite LiveJournal API
 
 ## Changelog
+
+### 0.3 (2017-01-08)
+Content export moved to separate plugin.
 
 ### 0.2.1 (2016-12-19)
 plugin.json updated.
 
-
 ### 0.2 (2016-11-28)
 - Support of `content_export` as plugin.
 - plugin.json updated.
-
 
 ### 0.1 (2016-11-23)
 First release.
--- __init__.py
+++ __init__.py
@@ -6,24 +6,3 @@ from ._session import Session
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
-
-
-def _init():
-    """Init wrapper.
-    """
-    from pytsite import lang, assetman
-    from ._content_export import Driver as ContentExportDriver
-
-    # Resources
-    lang.register_package(__name__, alias='livejournal')
-    assetman.register_package(__name__, alias='livejournal')
-
-    # Content export driver
-    try:
-        from plugins import content_export
-        content_export.register_driver(ContentExportDriver())
-    except ImportError as e:
-        raise RuntimeError("Required plugin is not found: {}".format(e))
-
-
-_init()
